f33c22a3586a9ad1e3ed8097576457aecddde7c1
ArfinWilly <arfinwilly5@gmail.com>
2025-12-29 20:04:01 +0700

    feat: setup dashboard layouts and app routing

---
+++ resources/views/layouts/dashboard/main.blade.php
@@ -0,0 +1,34 @@
+@include('layouts.dashboard.__header')
+
+<body>
+    <script src="{{ asset('assets/static/js/initTheme.js') }}"></script>
+
+    <div id="app">
+        @include('layouts.dashboard.__sidebar')
+        <div id="main">
+            <header class="mb-3">
+                <a href="#" class="burger-btn d-block d-xl-none">
+                    <i class="bi bi-justify fs-3"></i>
+                </a>
+            </header>
+
+            @yield('content')
+
+            @include('layouts.dashboard.__footer')
+        </div>
+    </div>
+
+    <script src="{{ asset('assets/static/js/components/dark.js') }}"></script>
+    <script src="{{ asset('assets/extensions/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
+
+
+    <script src="{{ asset('assets/compiled/js/app.js') }}"></script>
+
+    <!-- Need: Apexcharts -->
+    <script src="{{ asset('assets/extensions/apexcharts/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('assets/static/js/pages/dashboard.js') }}"></script>
+
+    @yield('script')
+</body>
+
+</html>
